74fbc2b44f935429e17c32452e818f63fc53ebfc
ahemberg <UfQRemBWB6fHxXtHV3Id>
2017-06-15 16:49:52 +0200

    Moved common header includes to one file to enable precompiling

---
+++ pch.h
@@ -0,0 +1,23 @@
+//
+// Created by A547832 on 2017-06-15.
+//
+
+/*
+ * PreCompiledHeader - Used to speed up compile time
+ */
+
+#ifndef TEMPSERVER_RMT_CPP_PCH_H
+#define TEMPSERVER_RMT_CPP_PCH_H
+
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <fstream>
+
+//JSON
+#include "json.hpp"
+
+//Defined Structs
+#include "structs.h"
+
+#endif //TEMPSERVER_RMT_CPP_PCH_H
