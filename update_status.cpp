09b3b88e4fa358c53c39ef778ed604a952468fe8
ahemberg <UfQRemBWB6fHxXtHV3Id>
2017-06-26 17:12:42 +0200

    Implemented functions for saving to local storage and sending to server

--- update_status.cpp
+++ update_status.cpp
@@ -21,8 +21,9 @@
 using namespace std;
 
 int main() {
-    SendStatusToServer send;
     board_parameters board;
+    vector<board_parameters> saved_measurements, received_measurements;
+    remote_info remote;
     db_auth sql_auth;
     string serial;
 
@@ -31,11 +32,18 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    //Read remote info from db
+    if (!get_remote_info(&sql_auth, &remote)) {
+        return EXIT_FAILURE;
+    }
+
+    remote.server_address += "/api/update_status";
+
     //Get RPI Serial
-    if (!get_rpi_serial(&serial)) {
+    if (!get_rpi_serial(&remote.board_serial)) {
         cerr << "Failed to get board serial" << endl;
     } else {
-        cout << "Board serial: " << serial << endl;
+        cout << "Board serial: " << remote.board_serial << endl;
     }
 
     //Get CPU temp
@@ -59,37 +67,40 @@ int main() {
 
     ping_server(&board.network);
 
-    /*Old procedure:
-     * 1 measure
-     * 2 save local
-     * 3 retrieve all local
-     * 4 send to server
-     * 5a remove saved
-     * 5b keep non saved
-     * Alternative Procedure:
-     * 1 measure
-     * 2 retrieve local
-     * 3 assemble old with new
-     * 4 send
-     * 5a remove saved
-     * 5b keep non saved
-     * 5c save current measurement if not saved on server
-     *
-     * PROS:
-     * No write to SD if all is successful!
-     * Probably faster
-     * CON:
-     * Measurement lost if there are any interruptions
-     * Need to handle no answer properly!
-     * Implementation more complicated (Maybe this is a PRO?)
-     */
+    //Get measurement timestamp and set id to 0
+    board.timestamp = sql_timestamp();
+    board.id = 0;
 
+    //TODO: This is heavy on the memory card. Will kill it after a year or two
+    //TODO: Do not save if not necessary Save only on fail
 
-    nlohmann::json board_info = send.create_status_blob(&board);
+    //Insert measurement to dB
+    save_status_message(&board, &sql_auth);
 
-    cout << board_info.dump(1) << endl;
+    //Retrieve all measurements from dB
+    saved_measurements = get_saved_status_messages(&sql_auth);
 
-    //Get available disk-space
-    return EXIT_SUCCESS;
+    SendStatusToServer send(remote, saved_measurements);
+
+    send.post_to_server(send.encoded_post);
+    send.parse_saved_messages();
+
+    cout << "Server status" << endl;
+    cout << send.server_response_code << endl;
+
+    cout << "Server Message" << endl;
+    cout << send.server_response_msg << endl;
+
+    if (send.server_response_code == 1) {
+        cout << "Server contact OK. Removing messages saved on server" << endl;
+        remove_status_messages(&sql_auth, send.server_saved_messages);
+        return EXIT_SUCCESS;
+    } else {
+        cerr << "Server contact FAIL. Local storage has been kept" << endl;
+        cerr << send.server_response_code << endl;
+        cerr << send.server_response_msg << endl;
+    }
+
+    return EXIT_FAILURE;
 }
 
