b1e83431b844c1fb79a9fd54236a9fbcc28efcd5
ahemberg <UfQRemBWB6fHxXtHV3Id>
2017-06-30 17:02:32 +0200

    Changed default path

--- lib/dbFunctions.cpp
+++ lib/dbFunctions.cpp
@@ -6,7 +6,7 @@
 
 #include "dbFunctions.h"
 
-bool load_db_param(db_auth *params) {
+bool load_db_param(db_auth *params, std::string path = "/tempserver_remote/cpp/secrets") {
     /*
      * Loads database authentication info from local secrets file. Params should point to a
      * db_auth struct. Secrets file has to be located in $HOME/tempserver_remote/cpp/
@@ -16,7 +16,7 @@ bool load_db_param(db_auth *params) {
      */
 
     std::string homeDir = getenv("HOME");
-    std::string filePath = "/tempserver_remote/cpp/secrets";
+    std::string filePath = path;
     nlohmann::json j;
 
     std::ifstream i(homeDir+filePath, std::ios::in);
@@ -108,112 +108,7 @@ bool get_remote_info(db_auth *auth, remote_info *rem) {
 
 }
 
-std::vector<saved_temp> get_saved_temperatures(db_auth *auth) {
 
-    std::string query = "SELECT * FROM saved_temp ORDER BY measurement_time DESC LIMIT 50";
-
-    try {
-        sql::Driver *driver;
-        sql::Connection *con;
-        sql::Statement *stmt;
-        sql::ResultSet *res;
-
-        driver = get_driver_instance();
-        con = driver->connect(auth->host, auth->user, auth->pwd);
-        con->setSchema(auth->database);
-
-        stmt = con->createStatement();
-        res = stmt->executeQuery(query);
-
-        int res_len = res->rowsCount();
-        int i = 0;
-
-        std::vector<saved_temp> rows(res_len);
-
-        while (res->next()) {
-            rows[i].id = res->getInt("id");
-            rows[i].timestamp = res->getString("measurement_time");
-            rows[i].temp = res->getDouble("temp");
-            i++;
-        }
-
-        delete res;
-        delete stmt;
-        delete con;
-        return rows;
-    } catch (sql::SQLException &e) {
-        std::cout << "# ERR: SQLException in " << __FILE__;
-        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
-        std::cout << "# ERR: " << e.what();
-        std::cout << " (MySQL error code: " << e.getErrorCode();
-        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-    }
-    std::vector<saved_temp> a;
-    return a;
-}
-
-bool save_temp(double temperature, db_auth *auth) {
-
-    try {
-        sql::Driver *driver;
-        sql::Connection *con;
-        sql::PreparedStatement *prep_stmt;
-
-        driver = get_driver_instance();
-        con = driver->connect(auth->host, auth->user, auth->pwd);
-        con->setSchema(auth->database);
-
-        prep_stmt = con->prepareStatement("INSERT into saved_temp (temp) VALUES (?)");
-
-        prep_stmt->setDouble(1,temperature);
-        prep_stmt->executeQuery();
-        delete(prep_stmt);
-        delete(con);
-
-    } catch (sql::SQLException &e) {
-        std::cout << "# ERR: SQLException in " << __FILE__;
-        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
-        std::cout << "# ERR: " << e.what();
-        std::cout << " (MySQL error code: " << e.getErrorCode();
-        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-        return false;
-    }
-    return true;
-}
-
-bool remove_temps(db_auth auth, std::vector<saved_temp> temps_to_remove) {
-
-    try {
-        sql::Driver *driver;
-        sql::Connection *con;
-        sql::PreparedStatement *prep_stmt;
-
-        driver = get_driver_instance();
-        con = driver->connect(auth.host, auth.user, auth.pwd);
-        con->setSchema(auth.database);
-
-        prep_stmt = con->prepareStatement("DELETE FROM saved_temp WHERE id = (?) AND measurement_time = (?)");
-
-        for (unsigned int i = 0; i < temps_to_remove.size(); i++) {
-            prep_stmt->setInt(1,temps_to_remove[i].id);
-            prep_stmt->setString(2,temps_to_remove[i].timestamp);
-            prep_stmt->execute();
-        }
-
-        delete(prep_stmt);
-        delete(con);
-
-    } catch (sql::SQLException &e) {
-        std::cout << "# ERR: SQLException in " << __FILE__;
-        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
-        std::cout << "# ERR: " << e.what();
-        std::cout << " (MySQL error code: " << e.getErrorCode();
-        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-        return false;
-    }
-    return true;
-
-}
 
 bool save_status_message(board_parameters *board_param, db_auth *auth) {
 
